47b6cffe8eb1d6be7e3e7efd40ee4e6eb38f692c
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2024-04-03 16:59:59 +0200

    refactoring code

--- client/src/components/ConfirmModal/index.js
+++ client/src/components/ConfirmModal/index.js
@@ -1,55 +1,61 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import { Typography, Modal } from '@mui/material';
+
+import { Typography, Modal, Box } from '@mui/material';
+
 import LinkFild from './LinkFild';
 import { SocialShare } from '../../components';
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 450,
-  bgcolor: 'background.paper',
-  border: '2px solid #b4b4b8',
-  borderRadius: 2,
-  boxShadow: 24,
-  p: 4,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 450,
+    bgcolor: 'background.paper',
+    border: '2px solid #b4b4b8',
+    borderRadius: 2,
+    boxShadow: 24,
+    p: 4,
 };
 const ConfirmModal = ({ title, link, onOpen, handleModalClose }) => {
-  return (
-    <div>
-      <Modal
-        open={onOpen}
-        onClose={handleModalClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography
-            id="modal-modal-title"
-            variant="h6"
-            mb={2}
-            component="h2"
-            sx={{ textAlign: 'center' }}
-          >
-            {title}
-          </Typography>
-          <LinkFild link={link} />
-          <Box
-            sx={{
-              mt: '10px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <SocialShare direction="row" useFlexGap title={title} link={link} />
-          </Box>
-        </Box>
-      </Modal>
-    </div>
-  );
+    return (
+        <div>
+            <Modal
+                open={onOpen}
+                onClose={handleModalClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography
+                        id="modal-modal-title"
+                        variant="h6"
+                        mb={2}
+                        component="h2"
+                        sx={{ textAlign: 'center' }}
+                    >
+                        {title}
+                    </Typography>
+                    <LinkFild link={link} />
+                    <Box
+                        sx={{
+                            mt: '10px',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <SocialShare
+                            sx={{ gap: '10px' }}
+                            direction="row"
+                            title={title}
+                            link={link}
+                        />
+                    </Box>
+                </Box>
+            </Modal>
+        </div>
+    );
 };
 
 export default ConfirmModal;
--- client/src/components/SocialChare/index.js
+++ client/src/components/SocialChare/index.js
@@ -1,35 +1,36 @@
-import { Box, Paper, Stack, styled } from '@mui/material';
+import { Stack } from '@mui/material';
+import { SOCIAL_BUTTON_SIZE } from '../../const';
 
 import {
-  EmailShareButton,
-  FacebookShareButton,
-  TelegramShareButton,
-  ViberShareButton,
-  WhatsappShareButton,
+    EmailShareButton,
+    FacebookShareButton,
+    TelegramShareButton,
+    ViberShareButton,
+    WhatsappShareButton,
 } from 'react-share';
 
 import { EmailIcon, FacebookIcon, TelegramIcon, ViberIcon, WhatsappIcon } from 'react-share';
 
 const SocialShare = ({ title, link, ...props }) => {
-  return (
-    <Stack sx={{ p: '5px', gap: '10px' }} {...props}>
-      <EmailShareButton subject={'Hove a nice list with movies: ' + String(title)} url={link}>
-        <EmailIcon size={32} round={true} />
-      </EmailShareButton>
-      <FacebookShareButton hashtag="#movieList" url={link}>
-        <FacebookIcon size={32} round={true} />
-      </FacebookShareButton>
-      <TelegramShareButton url={link}>
-        <TelegramIcon size={32} round={true} />
-      </TelegramShareButton>
-      <ViberShareButton url={link}>
-        <ViberIcon size={32} round={true} />
-      </ViberShareButton>
-      <WhatsappShareButton url={link}>
-        <WhatsappIcon size={32} round={true} />
-      </WhatsappShareButton>
-    </Stack>
-  );
+    return (
+        <Stack {...props}>
+            <EmailShareButton subject={'Hove a nice list with movies: ' + String(title)} url={link}>
+                <EmailIcon size={SOCIAL_BUTTON_SIZE} round={true} />
+            </EmailShareButton>
+            <FacebookShareButton hashtag="#movieList" url={link}>
+                <FacebookIcon size={SOCIAL_BUTTON_SIZE} round={true} />
+            </FacebookShareButton>
+            <TelegramShareButton url={link}>
+                <TelegramIcon size={SOCIAL_BUTTON_SIZE} round={true} />
+            </TelegramShareButton>
+            <ViberShareButton url={link}>
+                <ViberIcon size={SOCIAL_BUTTON_SIZE} round={true} />
+            </ViberShareButton>
+            <WhatsappShareButton url={link}>
+                <WhatsappIcon size={SOCIAL_BUTTON_SIZE} round={true} />
+            </WhatsappShareButton>
+        </Stack>
+    );
 };
 
 export default SocialShare;
