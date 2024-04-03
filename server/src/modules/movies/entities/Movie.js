07f1ae06e6876d6c59492ba3cc013f3800a733d9
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2024-04-03 16:35:18 +0200

    Update Movie.js

--- server/src/modules/movies/entities/Movie.js
+++ server/src/modules/movies/entities/Movie.js
@@ -11,11 +11,14 @@ export class Movie {
     this.adult = movie.adult;
     this.overview = movie.overview;
     this.originalLanguage = movie.original_language;
+    this.originalTitle = movie.original_title;
     this.backdropPath = `${config.IMAGE_BASE_PATH}${movie.backdrop_path}`;
     this.popularity = movie.popularity;
     this.voteCount = movie.vote_count;
     this.video = movie.video;
     this.voteAverage = movie.vote_average;
+    this.runtime = movie.runtime;
+    this.genres = movie.genres;
   }
   releaseDate(params) {
     try {
