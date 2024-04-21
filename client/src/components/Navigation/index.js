1b9e8a614d5f7eda6a26718ba0d749ec0ec85665
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2024-04-21 16:51:06 +0200

    refactoring navigaation

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -6,7 +6,6 @@ import {
     Toolbar,
     IconButton,
     Box,
-    Typography,
     Button,
     Drawer,
     List,
@@ -15,15 +14,33 @@ import {
     ListItemIcon,
     ListItemButton,
     Link,
+    Tabs,
+    Tab,
 } from '@mui/material';
+import SettingsTwoToneIcon from '@mui/icons-material/SettingsTwoTone';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import MenuIcon from '@mui/icons-material/Menu';
-import SettingsIcon from '@mui/icons-material/Settings';
-
+import SlideshowOutlinedIcon from '@mui/icons-material/SlideshowOutlined';
 import LanguageButton from './LanguageButton';
 import translate from '../../utils/translate';
 
+const NavLink = ({ to, children }) => {
+    return (
+        <Link
+            component={RouterLink}
+            to={to}
+            variant="button"
+            color="inherit"
+            sx={{ my: 1, mx: 2, textDecoration: 'none' }}
+        >
+            {children}
+        </Link>
+    );
+};
+
 const Navigation = () => {
     const [isDrowerOpen, setDrowerOpen] = useState(false);
+
     const list = () => (
         <Box sx={{ width: 250 }} role="presentation">
             <List>
@@ -31,7 +48,7 @@ const Navigation = () => {
                     <ListItem disablePadding>
                         <ListItemButton>
                             <ListItemIcon>
-                                <SettingsIcon />
+                                <SettingsTwoToneIcon />
                             </ListItemIcon>
                             <ListItemText primary="Settings" />
                         </ListItemButton>
@@ -41,10 +58,23 @@ const Navigation = () => {
         </Box>
     );
 
+    const [value, setValue] = useState('one');
+
+    const handleChange = (event, newValue) => {
+        console.log('newValue--', newValue);
+        console.log('event--', event);
+        setValue(newValue);
+    };
+
     return (
         <Box>
-            <AppBar position="static">
-                <Toolbar sx={{ maxWidth: 'calc(100vw - 2%)' }}>
+            <AppBar position="static" sx={{ backgroundColor: '#68ae03' }}>
+                <Toolbar
+                    sx={{
+                        maxWidth: 'calc(100vw - 2%)',
+                        display: 'flex',
+                    }}
+                >
                     <IconButton
                         onClick={() => setDrowerOpen(true)}
                         size="large"
@@ -55,19 +85,39 @@ const Navigation = () => {
                     >
                         <MenuIcon />
                     </IconButton>
-
-                    <Link component={RouterLink} to="/" sx={{ color: 'white', flexGrow: 1 }}>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            Recomendation films
-                        </Typography>
-                    </Link>
-                    <LanguageButton />
+                    <Tabs
+                        sx={{ borderTop: 1, borderColor: 'divider', flexGrow: '1' }}
+                        onChange={handleChange}
+                        value={value}
+                        component={'nav'}
+                        textColor="inherit"
+                        indicatorColor="inherit"
+                    >
+                        <Tab
+                            label="Home"
+                            value="one"
+                            icon={<HomeOutlinedIcon />}
+                            iconPosition="start"
+                            to="/"
+                            component={RouterLink}
+                        />
+                        <Tab
+                            label="Movies"
+                            value="two"
+                            icon={<SlideshowOutlinedIcon />}
+                            iconPosition="start"
+                            to="/movies"
+                            component={RouterLink}
+                        />
+                        <Tab label="About" value="three" to="/about" component={RouterLink} />
+                    </Tabs>
                     <Box
                         sx={{
                             display: { xs: 'none', md: 'flex' },
                         }}
                     >
-                        <Button component={RouterLink} to="settings" sx={{ my: 2, color: 'white' }}>
+                        <LanguageButton />
+                        <Button component={RouterLink} to="settings" sx={{ color: 'white' }}>
                             {translate('navigation.settings')}
                         </Button>
                     </Box>
@@ -81,3 +131,122 @@ const Navigation = () => {
 };
 
 export default Navigation;
+
+//--------------------------------------------------
+
+// import * as React from 'react';
+// import PropTypes from 'prop-types';
+// import Box from '@mui/material/Box';
+// import Tabs from '@mui/material/Tabs';
+// import Tab from '@mui/material/Tab';
+// import Typography from '@mui/material/Typography';
+// import { MemoryRouter, Route, Routes, Link, matchPath, useLocation } from 'react-router-dom';
+// import { StaticRouter } from 'react-router-dom/server';
+
+// function Router(props) {
+//     const { children } = props;
+//     if (typeof window === 'undefined') {
+//         return <StaticRouter location="/drafts">{children}</StaticRouter>;
+//     }
+
+//     return (
+//         <MemoryRouter initialEntries={['/drafts']} initialIndex={0}>
+//             {children}
+//         </MemoryRouter>
+//     );
+// }
+
+// Router.propTypes = {
+//     children: PropTypes.node,
+// };
+
+// function useRouteMatch(patterns) {
+//     const { pathname } = useLocation();
+
+//     for (let i = 0; i < patterns.length; i += 1) {
+//         const pattern = patterns[i];
+//         const possibleMatch = matchPath(pattern, pathname);
+//         if (possibleMatch !== null) {
+//             return possibleMatch;
+//         }
+//     }
+
+//     return null;
+// }
+
+// function MyTabs() {
+//     // You need to provide the routes in descendant order.
+//     // This means that if you have nested routes like:
+//     // users, users/new, users/edit.
+//     // Then the order should be ['users/add', 'users/edit', 'users'].
+//     const routeMatch = useRouteMatch(['/inbox/:id', '/drafts', '/trash']);
+//     const currentTab = routeMatch?.pattern?.path;
+
+//     return (
+//         <Tabs value={currentTab}>
+//             <Tab label="Inbox" value="/inbox/:id" to="/inbox/1" component={Link} />
+//             <Tab label="Drafts" value="/drafts" to="/drafts" component={Link} />
+//             <Tab label="Trash" value="/trash" to="/trash" component={Link} />
+//         </Tabs>
+//     );
+// }
+
+// function CurrentRoute() {
+//     const location = useLocation();
+
+//     return (
+//         <Typography variant="body2" sx={{ pb: 2 }} color="text.secondary">
+//             Current route: {location.pathname}
+//         </Typography>
+//     );
+// }
+
+// const Navigation = () => {
+//     return (
+
+//             <Box sx={{ width: '100%' }}>
+//                 <Routes>
+//                     <Route path="*" element={<CurrentRoute />} />
+//                 </Routes>
+//                 <MyTabs />
+//             </Box>
+
+//     );
+// };
+
+// export default Navigation;
+
+//------**************************************************************************
+
+// import * as React from 'react';
+// import Tabs from '@mui/material/Tabs';
+// import Tab from '@mui/material/Tab';
+// import Box from '@mui/material/Box';
+
+// export default function Navigation() {
+//     const [value, setValue] = React.useState(0);
+
+//     const handleChange = (event, newValue) => {
+//         setValue(newValue);
+//     };
+
+//     return (
+//         <Box sx={{ maxWidth: { xs: 320, sm: 480 }, bgcolor: 'background.paper' }}>
+//             <Tabs
+//                 value={value}
+//                 onChange={handleChange}
+//                 variant="scrollable"
+//                 scrollButtons="auto"
+//                 aria-label="scrollable auto tabs example"
+//             >
+//                 <Tab label="Item One" />
+//                 <Tab label="Item Two" />
+//                 <Tab label="Item Three" />
+//                 <Tab label="Item Four" />
+//                 <Tab label="Item Five" />
+//                 <Tab label="Item Six" />
+//                 <Tab label="Item Seven" />
+//             </Tabs>
+//         </Box>
+//     );
+// }
