a28c831ea505b2a4e7fa1cd1bd4f87866f23ce5b
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2024-04-21 16:42:40 +0200

    refactoring page Home

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -1,85 +1,60 @@
-import { useState } from 'react';
-import { useQuery } from '@apollo/client';
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
+import { useContext } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+// import { AuthContext, anonymousUser } from '../../AuthContext';
 
-import { Grid, Box, Paper, LinearProgress } from '@mui/material';
-import { ToastContainer } from 'react-toastify';
-
-import { MovieCard, MovieSelectedSection, PagePagination } from '../../components';
-import { useMovies } from '../../hooks/useMovies';
-import { MOVIES_QUERY } from './querys.js';
+function Copyright() {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center">
+            Copyright © The Movies DB {new Date().getFullYear()}
+        </Typography>
+    );
+}
 
 const Home = () => {
-    const [page, setPage] = useState(1);
-    const { loading, error, data } = useQuery(MOVIES_QUERY, { variables: { page } });
-    const { onCardDelete, onCardSelect, selectedMovies } = useMovies();
+    // const auth = useContext(AuthContext);
+    // const loggedIn = auth.user !== anonymousUser;
+    // const greeting = 'Explore movies today with us!';
 
-    if (error) return `Error! ${error}`;
-
-    const paginationHandler = (event, page) => {
-        setPage(page);
-    };
-
-    const movies = data ? data.movies.results : [];
-    const pageCount = data?.movies?.totalPages > 500 ? 500 : data?.movies?.totalPages;
-    const movieItems = movies.map(movie => (
-        <Grid
-            item
-            key={movie.id}
-            xs={12}
-            sm={6}
-            md={4}
-            xl={3}
-            sx={{ display: 'flex', justifyContent: 'center' }}
-        >
-            <MovieCard isLoading={loading} movie={movie} onCardSelect={onCardSelect} />
-        </Grid>
-    ));
     return (
-        <Box sx={{ flexGrow: 1, marginTop: 2 }}>
-            <Grid container spacing={2} sx={{ position: 'relative' }}>
-                <Grid item xs={12}>
-                    <Paper elevation={6}>Filters section</Paper>
-                </Grid>
-                <Grid item xs={12} md={9}>
-                    {/* <Paper elevation={6}> */}
-                    <Box sx={{ flexGrow: 1, padding: 1 }}>
-                        {loading && <LinearProgress />}
-                        {data && (
-                            <Grid container spacing={2}>
-                                {movieItems}
-                            </Grid>
-                        )}
-
-                        <PagePagination
-                            paginationHandler={paginationHandler}
-                            page={page}
-                            pageCount={pageCount}
-                        />
-                    </Box>
-                    {/* </Paper> */}
-                </Grid>
-                <Grid item xs={12} md={3} sx={{ position: 'sticky' }}>
-                    <MovieSelectedSection
-                        onCardDelete={onCardDelete}
-                        selectedMovies={selectedMovies}
-                        sx={{ maxWidth: '320px' }}
-                    />
-                </Grid>
-            </Grid>
-            <ToastContainer
-                position="bottom-left"
-                autoClose={2000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="colored"
-                stacked
-            />
+        <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 8 }}>
+            <Container maxWidth="sm">
+                <Typography
+                    component="h1"
+                    variant="h2"
+                    align="center"
+                    color="text.primary"
+                    gutterBottom
+                >
+                    Welcome
+                </Typography>
+                <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                    Explore movies today with us!
+                </Typography>
+                <Stack sx={{ pt: 4 }} direction="row" spacing={2} justifyContent="center">
+                    <Button
+                        component={RouterLink}
+                        to="/movies"
+                        variant="contained"
+                        color="secondary"
+                    >
+                        Explore
+                    </Button>
+                </Stack>
+            </Container>
+            <Container
+                maxWidth="md"
+                component="footer"
+                sx={{
+                    borderTop: theme => `1px solid ${theme.palette.divider}`,
+                    mt: 8,
+                    py: [3, 6],
+                }}
+            >
+                <Copyright />
+            </Container>
         </Box>
     );
 };
+
 export default Home;
