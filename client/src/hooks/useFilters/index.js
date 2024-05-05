a669637f0135f66d603f51ac95786f7f80a86923
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2024-05-05 22:06:31 +0200

    add useFilters

---
+++ client/src/hooks/useFilters/index.js
@@ -0,0 +1,41 @@
+import { useCallback, useState } from 'react';
+
+export const useFilters = () => {
+    const initialState = {
+        page: 1,
+        with_keywords: '',
+        sortDirection: 'asc',
+    };
+
+    const [filters, setFilterNormalize] = useState(initialState);
+
+    const setPage = useCallback(
+        page => {
+            setFilterNormalize({
+                ...filters,
+                page: page,
+            });
+        },
+        [filters]
+    );
+
+
+    const setFilter = useCallback(
+        filterFilds => {
+            setFilterNormalize({
+                ...filters,
+                ...filterFilds,
+                with_keywords: filterFilds.with_keywords.map(item => item.id).join('|'),
+                with_genres: filterFilds.with_genres?.length
+                    ? filterFilds.with_genres.join(',')
+                    : '',
+                with_runtime_gte: Number(filterFilds.with_runtime_gte),
+                with_runtime_lte: Number(filterFilds.with_runtime_lte),
+                primary_release_year: Number(filterFilds.primary_release_year),
+            });
+        },
+        [filters]
+    );
+
+    return { setPage, filters, setFilter };
+};
